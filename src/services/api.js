012f2a1a09ec2e78e42c1129d84d8e022515b9af
RahulGaniger <rahulganiger1709@gmail.com>
2026-06-06 14:59:30 +0530

    chnged the local port

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+console.log(import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
